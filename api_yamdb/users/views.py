f146e9e7112fa0176f455994ee2f4bf6c920596b
Никита Чередниченко <matador4.0@mail.ru>
2024-02-17 17:09:51 +0300

    minor changes

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
@@ -14,38 +14,6 @@ from .serializers import (
 )
 
 
-# class UserViewSet(viewsets.ModelViewSet):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-#     lookup_url_kwarg = 'username'
-#     permission_classes = (permissions.IsAdmin,)
-#     filter_backends = (DjangoFilterBackend, filters.SearchFilter,)
-#     search_fields = ('username',)
-#     http_method_names = ['get', 'post', 'delete', 'patch', ]
-
-#     def get_object(self):
-#         username = self.kwargs.get('username')
-#         return get_object_or_404(self.queryset, username=username)
-
-
-# class UserRetrieveUpdateViewSet(
-#                    mixins.RetrieveModelMixin,
-#                    mixins.UpdateModelMixin,
-#                    viewsets.GenericViewSet,
-# ):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-#     permission_classes = (IsAuthenticated,)
-#     def get_serializer_class(self):
-#         print('self.action______________________________', self.action)
-#         print('role____________________________', self.request.user.role)
-#         if self.action == 'update':
-#             return UserUpdateSerializer
-#         return UserSerializer
-
-#     def get_object(self):
-#         user = self.request.user
-#         return user
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
@@ -54,6 +22,7 @@ class UserViewSet(viewsets.ModelViewSet):
     filter_backends = (DjangoFilterBackend, filters.SearchFilter,)
     http_method_names = ('get', 'post', 'patch', 'delete')
     search_fields = ('username',)
+
     @action(
         detail=False,
         methods=('get', 'patch'),
@@ -73,10 +42,12 @@ class UserViewSet(viewsets.ModelViewSet):
             return Response(serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
 
+
 class RegistrationAPIView(APIView):
     """
     Разрешить всем пользователям (аутентифицированным и нет) доступ к данному эндпоинту.
     """
+
     serializer_class = RegistrationSerializer
     permission_classes = (AllowAny,)
 
@@ -88,7 +59,10 @@ class RegistrationAPIView(APIView):
                 return Response(serializer.data, status=status.HTTP_200_OK)
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
 
 class AuthenticationAPIView(APIView):
@@ -100,4 +74,7 @@ class AuthenticationAPIView(APIView):
         if serializer.is_valid():
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
