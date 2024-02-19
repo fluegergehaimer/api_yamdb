6a97f3ef3f0e05b7be115c9ddd866fa945779dfb
Никита Чередниченко <matador4.0@mail.ru>
2024-02-19 08:29:56 +0300

    users app deleted

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 from . import views
 
+
 app_name = 'api'
 
 router_v1 = DefaultRouter()
@@ -33,7 +34,38 @@ router_v1.register(
     views.TitlesViewSet,
     basename='titles'
 )
+router_v1.register(r'users', views.UserViewSet, basename='users',)
 
 urlpatterns = [
+    path(
+        'v1/auth/signup/',
+        views.RegistrationAPIView.as_view(),
+        name='signup'
+    ),
+    path(
+        'v1/auth/token/',
+        views.AuthenticationAPIView.as_view(),
+        name='token'
+    ),
     path('v1/', include(router_v1.urls)),
 ]
+
+
+
+###
+# from django.urls import include, path
+# from rest_framework import routers
+
+
+
+# app_name = 'users'
+
+# router_v1 = routers.DefaultRouter()
+# router_v1.register(r'', UserViewSet, basename='users',)
+
+# urlpatterns = [
+#     path('v1/auth/signup/', RegistrationAPIView.as_view(), name='signup'),
+#     path('v1/auth/token/', AuthenticationAPIView.as_view(), name='token'),
+#     path('v1/users/', include(router_v1.urls)),
+#     path('', include('api.urls')),
+# ]
